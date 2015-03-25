634f3c87c47786669297abd0065a004d32e432de
Anoop Elias <anoopelias@gmail.com>
2015-03-25 19:57:41 +0530

    Added some more test cases covering Separation, Cohesion & Alignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -16,7 +16,7 @@ describe('Boid', function() {
 
   var boid1 = {
     position: new Vector(0, 0),
-    speed: new Vector(0, 0),
+    speed: new Vector(0.5, 0.5),
     acceleration: new Vector(0, 0)
   };
   var boid2 = {
@@ -26,6 +26,11 @@ describe('Boid', function() {
   };
   var boid3 = {
     position: new Vector(60, 60),
+    speed: new Vector(0, 0),
+    acceleration: new Vector(0, 0)
+  };
+  var boid4 = {
+    position: new Vector(-10, -10),
     speed: new Vector(0, 0),
     acceleration: new Vector(0, 0)
   };
@@ -43,6 +48,63 @@ describe('Boid', function() {
     var boids = new Boids(defaultOptions);
     boids.boids = [boid1, boid3];
     var sep = boids.calcSeparation(boid1);
+    assert.equal(sep.x, 0);
+    assert.equal(sep.y, 0);
+  });
+
+  it('should have non-zero separation for boids behind them', function() {
+    var boids = new Boids(defaultOptions);
+    boids.boids = [boid1, boid4];
+    var sep = boids.calcSeparation(boid1);
+    assertApprox(sep.x, -0.0212, 4);
+    assertApprox(sep.y, -0.0212, 4);
+
+  });
+
+  it('should calculate cohesion', function() {
+    var boids = new Boids(defaultOptions);
+    boids.boids = [boid1, boid2];
+    var sep = boids.calcCohesion(boid1);
+    assertApprox(sep.x, 0.0212, 4);
+    assertApprox(sep.y, 0.0212, 4);
+  });
+
+  it('should have zero cohesion for far away boids', function() {
+    var boids = new Boids(defaultOptions);
+    boids.boids = [boid1, boid3];
+    var sep = boids.calcCohesion(boid1);
+    assert.equal(sep.x, 0);
+    assert.equal(sep.y, 0);
+  });
+
+  it('should have zero cohesion for boids behind them', function() {
+    var boids = new Boids(defaultOptions);
+    boids.boids = [boid1, boid4];
+    var sep = boids.calcCohesion(boid1);
+    assert.equal(sep.x, 0);
+    assert.equal(sep.y, 0);
+  });
+
+  it('should calculate alignment', function() {
+    var boids = new Boids(defaultOptions);
+    boids.boids = [boid1, boid2];
+    var sep = boids.calcAlignment(boid1);
+    assertApprox(sep.x, -0.0212, 4);
+    assertApprox(sep.y, -0.0212, 4);
+  });
+
+  it('should have zero alignment for far away boids', function() {
+    var boids = new Boids(defaultOptions);
+    boids.boids = [boid1, boid3];
+    var sep = boids.calcAlignment(boid1);
+    assert.equal(sep.x, 0);
+    assert.equal(sep.y, 0);
+  });
+
+  it('should have zero alignment for boids behind them', function() {
+    var boids = new Boids(defaultOptions);
+    boids.boids = [boid1, boid4];
+    var sep = boids.calcAlignment(boid1);
     assert.equal(sep.x, 0);
     assert.equal(sep.y, 0);
   });
