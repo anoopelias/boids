cf710523196686003c4acc86b513bb2822eaa0b8
Anoop Elias <anoopelias@gmail.com>
2015-03-29 11:05:24 +0530

    Updated benchmark for warmup

--- js/benchmark.js
+++ js/benchmark.js
@@ -2,14 +2,18 @@ function benchmark(boids) {
   function test(count, ticks) {
     var b = boids({ boids: count }),
       i = ticks,
-      start = +new Date();
+      start;
 
+    var warmup = 10000;
+    while (warmup--) b.tick();
+
+    start = +new Date();
     while (i--) b.tick();
 
     return ticks / ((new Date() - start) / 1000);
   }
 
-  for (var i = 50; i <= 1000; i += 50) {
+  for (var i = 50; i <= 500; i += 50) {
     console.log(i + ' boids: ' + ~~test(i, 5000) + ' ticks/sec');
   }
 }
@@ -55,5 +59,5 @@ function dtreeBenchmark(Dtree) {
 }
 
 
-//benchmark(require('./'));
-dtreeBenchmark(require('./dtree'));
+benchmark(require('./'));
+//dtreeBenchmark(require('./dtree'));
