04c51638083852428d46eae2795c23db3d0047ca
Anoop Elias <anoopelias@gmail.com>
2019-03-24 23:54:07 +0530

    Upgraded versions
    Signed-off-by: Anoop Elias <anoopelias@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
         template: 'index.html',
         favicon: 'favicon.ico',
