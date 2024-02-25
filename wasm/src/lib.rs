45883711333c32933d2c8228bbcd57e4b72c7e7a
Anoop Elias <anoopelias@gmail.com>
2024-02-26 00:59:22 +0530

    Convert alert to `console.log`

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -5,9 +5,12 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, wasm!");
+    log("Hello, wasm!");
 }
+
